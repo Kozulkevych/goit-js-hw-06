620792f9141dbe2caa60657e92ebbd4705dd5fac
Kozulkevych Nadiia <nkozulkevych@gmail.com>
2023-01-11 19:40:51 +0200

    виправлення помилок

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,15 @@
 const inputRef = document.querySelector('#validation-input');
 
-// console.log(inputRef.dataset.length);
+
 
 inputRef.addEventListener('blur', event => {
+    inputRef.classList.add('invalid');
     if (event.target.value.length === Number(inputRef.dataset.length)) {
         inputRef.classList.remove('invalid');
         inputRef.classList.add('valid');
     }
-    else {
-        inputRef.classList.remove('valid');
-        inputRef.classList.add('invalid');
-    }
     // console.log(event.target.value.length);
+    // console.log(Number(inputRef.dataset.length));
  });
 // function onInputRefFocus() { };
 // function onInputRefBlur() { };
